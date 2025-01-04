6eb3c073c0f4a6d5a067e3e9118ddb30bf11b4d7
YAHYA AYKAN <yahyaaykan.1@gmail.com>
2025-01-04 13:30:03 +0300

    +gameplaycontroller and menuscreen

---
+++ Assets/Scripts/DataController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class DataController : MonoBehaviour
+{
+    public static DataController ornek;
+    private const string High_Score = "High Score";
+    // Start is called before the first frame update
+    void Start()
+    {
+        TekilNesne();
+    }
+    void TekilNesne()
+    {
+        if (ornek != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            ornek = this;
+            DontDestroyOnLoad(ornek);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Oyunilkdefabasladi()
+    {
+        PlayerPrefs.SetInt(High_Score, 0);
+        PlayerPrefs.SetInt("Oyunilkdefabasladi", 0);
+
+    }
+    public void setHighScore(int score)
+    {
+        PlayerPrefs.SetInt(High_Score, score);
+    }
+    public int getHighScore()
+    {
+        return PlayerPrefs.GetInt(High_Score);
+    }
+}
---
+++ Assets/Scripts/GamePlayController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GamePlayController : MonoBehaviour
+{
+    public static GamePlayController ornek;
+    // Start is called before the first frame update
+    [SerializeField]
+    private Text scoreText, endscoreText, bestScoreText, gameOverText;
+    [SerializeField]
+    private Button restartButton, PauseButton;
+    [SerializeField]
+    private GameObject pausePanel;
+    [SerializeField]
+    private Image[] medalicon;
+    private void Awake()
+    {
+        
+    }
+    private void PauseGame()
+    {
+
+    }
+    private void goToMenuButton()
+    {
+
+    }
+    private void RestartGame()
+    {
+
+    }
+    private void ResumeGame()
+    {
+
+    }
+    private void PlayGame()
+    {
+
+    }
+    private void SetScore(int score)
+    {
+
+    }
+    private void SkoruGoster(int score)
+    {
+
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
